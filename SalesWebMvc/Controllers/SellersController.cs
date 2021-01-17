3ec25149b23173ede7e152dea34f312b506eedc9
Victor Michel <victor.michel@shieldsec.com.br>
2021-01-17 19:35:06 -0300

    Delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
@@ -41,6 +37,30 @@ namespace SalesWebMvc.Controllers
         {
             _sellerService.Insert(seller);
 
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id) 
+        {
+            if (id == null) 
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null) 
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id) 
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
